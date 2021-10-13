ff794a345e7bc50be45e3b84db0388cc08819d20
Elad Bergrin <EladB1@users.noreply.github.com>
2021-10-13 00:23:35 -0400

    Added basic HTML and CSS of first component. Still need to make styling tweaks before continuing.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import './Project.css';
+
+const ProjectTitle = (props) => {
+
+};
+
+const ProjectDescription = (props) => {
+
+};
+
+const ProjectTimeFrame = (props) => {
+
+};
+
+const ProjectTags = (props) => {
+
+};
+
+const Project = (props) => {
+    return (
+        <div class="project">
+          <p id="title">Title</p>
+          <p id="time">June 2019-Oct. 2019</p>
+          <hr id="sep1"/>
+          <p id="descr">Description of project that took X amount of time to complete and involved doing something or another</p>
+          <hr id="sep2"/>
+          <div id="tags">
+            <p class="tag">Python</p>
+            <p class="tag">MongoDB</p>
+          </div>
+        </div>
+    );
+};
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +50,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>*/}
+        <div id="projectDisplay">
+            <Project />
+            <Project />
+        </div>
       </header>
     </div>
   );
